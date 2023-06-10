bf72f3f4e140261400c4815dae72c1fb74b72d09
Eko Yanuarso Budi <36530602+ekoyanu99@users.noreply.github.com>
2023-06-10 10:35:32 +0700

    styling responsive

--- src/components/Tokens.js
+++ src/components/Tokens.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Inch from "../assets/1inch-logo.svg";
 
 function Tokens() {
   return (
-    <div>Tokens</div>
-  )
+        <>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <img src={Inch} alt="logo" className="Token-Logo" />
+                <div className='total-supply'>
+                    <h1>Total Supply:
+                        1,500,000,000
+                    </h1>
+                </div>
+                <div>
+                    <h2>Welcome to the 1INCH Token</h2>
+                    <p className='token-info'>
+                        The 1INCH token is the governance and utility token of the 1inch Network. 
+                        1INCH holders can stake their tokens to participate in the 1inch Network's DAO governance, including the 1inch DAO Treasury management. 
+                        Also, holders can choose resolvers and earn a part of their fees.
+                    </p>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Tokens
